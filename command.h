b2a262fb78e7ff6c12d70060326bd14fc453cb63
sergeystarkov <starkov.sa@gmail.com>
2016-10-20 15:32:12 +0400

    Перечисление комманд
    список комманд процессора. Структура команд с указателем на реализацию
    комманды и её длинной

--- command.h
+++ command.h
@@ -1,4 +1,20 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
+
+//Класс реализует выполнение опкодов
+class commands
+{
+public:
+    commands(){}
+    ~commands(){}
+
+    static int op_1(){return 1;}//реализацию пожже вынести из тела класса, оставить только объявления
+    static int op_2(){return 1;}//..
+    static int op_3();//..
+};
+
+
+
 #endif // COMMAND_H
+
--- operation.h
+++ operation.h
@@ -1,4 +1,8 @@
 #ifndef OPERATION_H
 #define OPERATION_H
 
+enum Operations {
+    OP_1 = 0x00
+};
+
 #endif // OPERATION_H
