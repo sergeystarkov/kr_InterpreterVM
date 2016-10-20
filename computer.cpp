55181f06bc35d8deb3645b37703c376f2ecfce6d
sergeystarkov <starkov.sa@gmail.com>
2016-10-20 20:41:04 +0400

    Revert "Revert "Перечисление комманд""
    This reverts commit 2163e6d3d9cae4a0c93bbb20244641d45742cbb0.

--- computer.cpp
+++ computer.cpp
@@ -1,2 +1,19 @@
 #include "computer.h"
 
+
+Computer::CMD::CMD(uByte LEN, int (*FOO)())
+{
+    Length = LEN;
+    pOP = FOO;
+}
+
+Computer::Computer()
+{
+    Cmd[OP_1] = CMD(2,commands::op_1);
+
+}
+
+Computer::~Computer()
+{
+
+}
--- computer.h
+++ computer.h
@@ -1,4 +1,24 @@
 #ifndef COMPUTER_H
 #define COMPUTER_H
 
+#include "operation.h"
+#include "types.h"
+#include "command.h"
+
+class Computer
+{
+public:
+    Computer();
+    ~Computer();
+
+    friend class com;
+    struct CMD{
+       uByte Length; // длина команды
+       int (*pOP)();
+       CMD(uByte LEN, int (*FOO)());
+       CMD(){}
+    };
+    CMD Cmd[256]; // основной набор команд
+};
+
 #endif // COMPUTER_H
